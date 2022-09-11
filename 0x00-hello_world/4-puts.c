273b09aa17a6a1e03ffd8986aaec8ea8034a34f2
Modalegend1 <yo[C>
2022-09-11 10:39:48 -0700

    task 1 commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<s t d i o . h>
+/**
+ * main- printing
+ * 
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
